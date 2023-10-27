7afd67a3e497887cfa962dc77a66871ef62feea0
Vaibhav <vaibhavpanwalkar39@gmail.com>
2023-10-27 11:47:40 +0530

    all files uploaded

---
+++ src/main/java/com/element/SpringMvcDemo27octApplication.java
@@ -0,0 +1,13 @@
+package com.element;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcDemo27octApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcDemo27octApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/element/SpringMvcDemo27octApplicationTests.java
@@ -0,0 +1,13 @@
+package com.element;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcDemo27octApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
